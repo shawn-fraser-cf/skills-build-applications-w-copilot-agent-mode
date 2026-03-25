972069ae691b2361064083f6f0ca65bc1d77179d
shawn-fraser-cf <shawn.fraser@createfuture.com>
2026-03-25 14:51:14 +0000

    After update DB

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    team = models.CharField(max_length=100)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    members = models.JSONField(default=list)
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    activity = models.CharField(max_length=100)
+    duration = models.IntegerField()
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
